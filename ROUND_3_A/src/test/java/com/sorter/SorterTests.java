efea322afc044d798a9f29f787915c60df0e14ec
Nicola <nicola_rushton@outlook.com>
2022-02-02 16:01:49 +0000

    refactor round 3

--- ROUND_3_A/src/test/java/com/sorter/SorterTests.java
+++ ROUND_3_A/src/test/java/com/sorter/SorterTests.java
@@ -1,7 +1,5 @@
 package com.sorter;
 
-import com.sorter.SortKind;
-import com.sorter.Sorter;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -12,21 +10,21 @@ public class SorterTests {
     @Test
     public void bubblesortPutsArrayInAscendingOrder() {
         int[] array = {3, 1, 2};
-        array = new Sorter().sort(SortKind.BUBBLE, array);
+        array = new Sorter().sort(new BubbleSort(), array);
         assertArrayEquals(new int[]{1, 2, 3}, array);
     }
 
     @Test
     public void quicksortPutsArrayInAscendingOrder() {
         int[] array = {3, 1, 2};
-        array = new Sorter().sort(SortKind.QUICK, array);
+        array = new Sorter().sort(new QuickSort(), array);
         assertArrayEquals(new int[]{1, 2, 3}, array);
     }
 
     @Test
     public void insertionsortPutsArrayInAscendingOrder() {
         int[] array = {3, 1, 2};
-        array = new Sorter().sort(SortKind.INSERTION, array);
+        array = new Sorter().sort(new InsertionSort(), array);
         assertArrayEquals(new int[]{1, 2, 3}, array);
     }
 
